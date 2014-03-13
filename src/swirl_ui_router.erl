bef2c5b4f7c816a60a0f4a8af6a10514b398787a
Louis-Philippe Gauthier <lpgauth@gmail.com>
2014-03-13 01:57:53 -0400

    Fix spacing

--- src/swirl_ui_router.erl
+++ src/swirl_ui_router.erl
@@ -9,13 +9,13 @@
 
 %% cowboy_http_handler callbacks
 init(_Type, Req, []) ->
-	{ok, Req, undefined}.
+    {ok, Req, undefined}.
 
 handle(Req, State) ->
     Headers = [{<<"content-type">>, <<"text/html">>}],
     {ok, Body} = index_dtl:render([]),
-	{ok, Req2} = cowboy_req:reply(200, Headers, Body, Req),
-	{ok, Req2, State}.
+    {ok, Req2} = cowboy_req:reply(200, Headers, Body, Req),
+    {ok, Req2, State}.
 
 terminate(_Reason, _Req, _State) ->
-	ok.
+    ok.
